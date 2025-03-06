ae703a84e9755130d049a23d8c06a0cf527ade2b
AurelienPospieszynski <aurelien.pos@hotmail.fr>
2025-03-07 00:09:58 +0100

    Creating home component

---
+++ frontend/moneytrio/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  standalone: true,
+})
+export class HomeComponent {
+  title = 'moneytrio';
+}
